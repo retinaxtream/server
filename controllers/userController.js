61889a6f3ba720f711bd9dd8c5823ff1859cc836
Adarsh <adarshkmeethal@gmail.com>
2023-09-18 16:45:30 +0530

    created the router for image upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,21 @@
+import path from 'path';
+// import multer from 'multer';
+import { Storage } from '@google-cloud/storage';
+import sharp from 'sharp';
+const currentModuleUrl = new URL(import.meta.url);
+const currentModuleDir = path.dirname(currentModuleUrl.pathname);
+
+const serviceAccPath = path.resolve(currentModuleDir, '../credentials.json');    
+
+const keyFilename = 'C:/Users/ADARSH/Desktop/Retina.x/credentials.json'
+
+
+const storage = new Storage({
+    projectId: "primal-stock-396615",
+    keyFilename: keyFilename,  
+  });   
+  
+  const bucketName = 'zephyrgllide'; 
 
 
 export const userWelcome = (req, res) => {
@@ -9,4 +27,103 @@ export const userWelcome = (req, res) => {
 };
 
 
+// export const uploadImage = async(req, res) => {
+//     try {
+//         console.log('calling');
+//         if (!req.files || req.files.length === 0) {
+//           return res.status(400).json({ error: 'No image files provided' });
+//         }
+        
+//         const imageUrls = [];
+    
+//         const bucket = storage.bucket(bucketName);
 
+//         for (const imageFile of req.files) {
+//           const clientName = 'ClientA'; // Replace with the actual client information
+//           const imageName = `${clientName}/${imageFile.originalname}`;
+//           // const imageName = imageFile.originalname; 
+//           const blob = bucket.file(imageName); 
+    
+//           // const blobStream = blob.createWriteStream({
+//           //   resumable: false,
+//           // }); 
+    
+//           const blobStream = blob.createWriteStream({
+//             metadata: {
+//               contentType: imageFile.mimetype,
+//               metadata: {     
+//                 client: 'ClientA',
+//               },
+//             },
+//             resumable: false,
+//           });
+    
+//           blobStream.on('finish', () => {
+//             const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+//             imageUrls.push(publicUrl);
+    
+//             if (imageUrls.length === req.files.length) {
+//               res.status(200).json({ message: 'Images uploaded successfully', imageUrls });
+//             }
+//           });
+
+//           blobStream.end(imageFile.buffer);
+//         }
+
+//       } catch (error) {   
+//         console.error(error);
+//         res.status(500).json({ error: 'An error occurred' });
+//       }
+//   };
+
+
+
+export async function uploadImage(req, res) {
+  try {
+
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ error: 'No image files provided' });
+    }
+
+    const imageUrls = [];
+    const bucket = storage.bucket(bucketName);
+
+    for (const imageFile of req.files) {
+      const clientName = 'ClientA'; 
+      const imageName = `${clientName}/${imageFile.originalname}`;
+      const blob = bucket.file(imageName);
+
+      // const optimizedImageBuffer = await sharp(imageFile.buffer)
+      // .jpeg({ quality: 50 }) 
+      // .toBuffer();
+
+      const blobStream = blob.createWriteStream({
+        metadata: {
+          contentType: imageFile.mimetype,
+          metadata: {
+            client: 'ClientA',
+          },
+        },
+        resumable: false,
+      });
+
+      blobStream.on('finish', () => {
+        const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+        imageUrls.push(publicUrl);
+
+        if (imageUrls.length === req.files.length) {
+          res.status(200).json({ message: 'Images uploaded successfully', imageUrls });
+        }
+      });
+
+      blobStream.end(imageFile.buffer);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+}
+
+
+
+ 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import * as userController from '../controllers/userController.js';
+import multer from 'multer';
+
+const multerStorage = multer.memoryStorage();
+const upload = multer({ storage: multerStorage });
 
 const router = express.Router();
 
 router
-    .route('/')
-    .get(userController.userWelcome)    
+    .route('/')        
+    .get(userController.userWelcome);
 
+// Import the 'upload' middleware
+// import { upload } from '../controllers/userController.js';
 
-export default router
-      
+router
+    .route('/upload/images')
+    .post(upload.array('images'), userController.uploadImage);
+
+export default router;
