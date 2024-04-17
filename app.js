7a9f79e0a8f2a21843d01c8ab4c1c8fd1ec78e8e
Adarsh <adarshkmeethal@gmail.com>
2024-04-17 13:51:40 +0530

    cors error

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
+import userroute from './routes/userRoutes.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userroute from './routes/userRoutes.js';
 import connectDatabase from './config/mongodb.js';
 
 dotenv.config({ path: './config.env' });
@@ -11,32 +11,31 @@ connectDatabase();
 const app = express();
 app.use(express.json());
 
-// Configure Morgan Logger middleware
+console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Custom Middleware
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
 });
 
-// CORS Configuration
-app.use(cors({  
-  origin: ['https://api.hapzea.com','https://hapzea.com','https://hapzea.com/'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Content-Type-Options'],
-  credentials: true,
-}));
+app.use(
+  cors({  
+    origin: [
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Content-Type-Options'],
+    credentials: true,
+  })
+);
 
-// Routes
 app.use('/api/v1/user', userroute);
 
-// Get the port from environment variable
-const port = process.env.PORT || 3000;
-
-// Start the server
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
